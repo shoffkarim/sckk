5562dd73ed042ee4899bb9cf23b323891aad6634
shoffkarim <k4r1mch1k@gmail.com>
2021-03-29 13:21:37 +0500

    верстка первого экрана

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -1,2 +1,13 @@
+/* eslint-disable no-undef */
 // STYLES=============================================================================
-import '../stylus/style.styl';
+import '../sass/style.sass';
+
+// SWIPER
+// eslint-disable-next-line no-unused-vars
+let mainSlider = new Swiper('.main-slider', {
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
