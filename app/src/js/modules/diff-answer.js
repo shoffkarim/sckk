e2e257afa722078ab1595ab59ae936c1ec00482e
shoffkarim <k4r1mch1k@gmail.com>
2021-04-14 15:36:31 +0500

    поправил свой вариант

--- app/src/js/modules/diff-answer.js
+++ app/src/js/modules/diff-answer.js
@@ -6,20 +6,22 @@ class DiffAnswer {
 
   init() {
     if (document.querySelector(".js-diff-wrap")) {
-      const container = document.querySelector(".js-diff-wrap");
-      const name = container.dataset.name;
-      const radios = container.querySelectorAll(`input[name=${name}]`);
-      radios.forEach((i) => i.addEventListener("change", function () {
-        if (i.parentElement.parentElement.classList.contains("js-diff-answer")) {
-          const wrap = container.querySelector(".js-diff-answer");
-          const input = wrap.querySelector(".js-diff-input");
-          input.classList.add("open");
-        } else {
-          const wrap = container.querySelector(".js-diff-answer");
-          const input = wrap.querySelector(".js-diff-input");
-          input.classList.remove("open");
-        }
-      }));
+      const container = document.querySelectorAll(".js-diff-wrap");
+      container.forEach(function (i) {
+        const name = i.dataset.name;
+        const radios = i.querySelectorAll(`input[name=${name}]`);
+        radios.forEach((j) => j.addEventListener("change", function () {
+          if (j.parentElement.parentElement.classList.contains("js-diff-answer")) {
+            const wrap = container.querySelector(".js-diff-answer");
+            const input = wrap.querySelector(".js-diff-input");
+            input.classList.add("open");
+          } else {
+            const wrap = container.querySelector(".js-diff-answer");
+            const input = wrap.querySelector(".js-diff-input");
+            input.classList.remove("open");
+          }
+        }));
+      });
     }
   }
 }
